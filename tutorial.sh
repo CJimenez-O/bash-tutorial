9e5b807850e62a2af252ce3c5eca20acf036e4cd
Chris Jimenez <cjimenez@partsauthority.com>
2023-03-20 16:10:06 -0700

    second commit

--- tutorial.sh
+++ tutorial.sh
@@ -53,7 +53,57 @@ count=9
 # {0..10..2} loop by 2 
 # for i in {0..10}
 
-for(( i=0; i<5; i++))
-do 
-    echo "$i"
-done
+# for(( i=0; i<5; i++))
+# do 
+#     echo "$i"
+# done
+
+# using $0 gives file name
+# echo $1 $2 $3
+
+# @ all 
+args=("$@")
+# echo ${args[0]} ${args[1]} ${args[2]}
+
+# echos all inputs
+# echo $@ 
+
+# prints length of array
+# echo $#
+
+# -p: prompt
+# -sp: silent prompt
+
+# echo Please sign-in
+# read -p 'Username: ' Username
+# read -sp 'Password: ' Password
+# echo
+# echo Welcome $Username
+
+
+# read files
+#  while read line
+#  do 
+#     echo "$line"
+# done < "${1:-/dev/stdin}"
+
+
+# 1: 
+# ls -al 1>standardOutput.txt 2>standardError.txt
+# ls +al 1>standardOutput.txt 2>standardError.txt
+
+
+
+# get date
+# Year=`date +%Y`
+# Month=`date +%m`
+# Day=`date +%d`
+# Hour=`date +%H`
+# Minute=`date +%M`
+# Second=`date +%S`
+# echo `date`
+# echo "Current Date is: $Day-$Month-$Year"
+
+date
+
+ping -c 4 172.18.95.10 
